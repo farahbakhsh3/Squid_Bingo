ce734560d8496fbfe4c0c76eb7da4063269ccc0b
Aliac <123499611+Aliac8888@users.noreply.github.com>
2024-01-14 21:11:21 +0330

    Update BingoBoard.java
    Additional Method for Checking Marked Status, Encapsulation, Improve the win Method.

--- Ali_Mehdizadeh/BingoBoard.java
+++ Ali_Mehdizadeh/BingoBoard.java
@@ -2,17 +2,18 @@ package squid_bingo;
 
 public class BingoBoard {
 
+    private static final int BOARD_SIZE = 5;
     private int[][] data;
     private int[][] marked;
 
     public BingoBoard(int[][] data) {
         this.data = data;
-        this.marked = new int[5][5];
+        this.marked = new int[BOARD_SIZE][BOARD_SIZE];
     }
 
-    public void marked(int num) {
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
+    public void mark(int num) {
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            for (int j = 0; j < BOARD_SIZE; j++) {
                 if (this.data[i][j] == num) {
                     this.marked[i][j] = 1;
                 }
@@ -21,40 +22,34 @@ public class BingoBoard {
     }
 
     public boolean win() {
-        for (int i = 0; i < 5; i++) {
-            boolean won = true;
-            for (int j = 0; j < 5; j++) {
-                if (this.marked[i][j] == 0) {
-                    won = false;
-                    break;
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            boolean rowWon = true;
+            boolean colWon = true;
+
+            for (int j = 0; j < BOARD_SIZE; j++) {
+                if (marked[i][j] == 0) {
+                    rowWon = false;
+                }
+
+                if (marked[j][i] == 0) {
+                    colWon = false;
                 }
             }
-            if (won) {
+
+            if (rowWon || colWon) {
                 return true;
             }
         }
 
-        for (int j = 0; j < 5; j++) {
-            boolean won = true;
-            for (int i = 0; i < 5; i++) {
-                if (this.marked[i][j] == 0) {
-                    won = false;
-                    break;
-                }
-            }
-            if (won) {
-                return true;
-            }
-        }
         return false;
     }
 
     public int score(int called) {
         int score = 0;
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
-                if (this.marked[i][j] == 0) {
-                    score += this.data[i][j];
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            for (int j = 0; j < BOARD_SIZE; j++) {
+                if (marked[i][j] == 0) {
+                    score += data[i][j];
                 }
             }
         }
@@ -62,12 +57,24 @@ public class BingoBoard {
     }
 
     public void draw() {
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
-                System.out.print(this.data[i][j] + " ");
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            for (int j = 0; j < BOARD_SIZE; j++) {
+                System.out.print(data[i][j] + " ");
             }
             System.out.println(" ");
         }
         System.out.println("--------------");
+    }
+
+    public boolean isMarked(int row, int col) {
+        return marked[row][col] == 1;
+    }
+
+    public int[][] getData() {
+        return data;
+    }
+
+    public int[][] getMarked() {
+        return marked;
     }
 }
